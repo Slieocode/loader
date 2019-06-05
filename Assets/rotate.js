651923f9a758dba50cbbb73b6b7ddd81ee2085c0
Slieo <slimanmad1@gmail.com>
2019-06-06 03:42:51 +0900

    Update rotate.js

--- Assets/rotate.js
+++ Assets/rotate.js
@@ -313,3 +313,15 @@ $(window).load(function(){
     });
 });
 })(jQuery);
+
+$(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
+$(window).load(function() {
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+});
+
+
+
